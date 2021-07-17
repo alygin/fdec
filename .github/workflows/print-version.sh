0c4ea02301f969e501899f1ea17a61f69d431c62
Andrew Lygin <alygin@gmail.com>
2021-07-17 18:30:51 +0300

    Fix version printing, #9

--- .github/workflows/print-version.sh
+++ .github/workflows/print-version.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sdir="$(dirname "$0")"
-sed -n 's/^version = \"\(.*\)\"/\1/p' < $sdir/../../Cargo.toml
+ver="$(sed -n 's/^version = \"\(.*\)\"/\1/p' < $sdir/../../Cargo.toml)"
+echo ::set-output name=version::$ver
